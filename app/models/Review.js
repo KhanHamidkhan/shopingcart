a5a2cf060a4ac8c6226e5feebbc189affb95fade
Nauman <naumanahmad17@gmail.com>
2016-12-28 02:42:45 -0800

    delete review

--- app/models/Review.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-// set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('Reviews', new Schema({
-      user:{type:Schema.ObjectId,ref:'User'},
-    products:[{type:Schema.ObjectId,ref:'Product'}],
-    created_at:{type:Date}
-}));
